3a043136ac2691555e8832d3ab243bb9e7acb803
Kyruski <jamescboyett@gmail.com>
2019-01-03 15:31:26 -0600

    Implemented _.map

--- src/underbar.js
+++ src/underbar.js
@@ -112,6 +112,12 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    let copyArray = collection.slice();
+    let tempArray = [];
+    for (let i = 0; i < copyArray.length; i++) {
+      tempArray.push(iterator(copyArray[i]));
+    }
+    return tempArray;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
